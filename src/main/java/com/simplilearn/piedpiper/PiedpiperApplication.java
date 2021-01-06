ddd0a40f9f9aef58591e05bcd48019056e2ace0b
Ram Iyer <ramky.iyer@gmail.com>
2021-01-06 15:45:08 -0500

    simple hello world with junit tests

--- src/main/java/com/simplilearn/piedpiper/PiedpiperApplication.java
+++ src/main/java/com/simplilearn/piedpiper/PiedpiperApplication.java
@@ -2,12 +2,16 @@ package com.simplilearn.piedpiper;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.joda.time.LocalTime;
 
 @SpringBootApplication
 public class PiedpiperApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(PiedpiperApplication.class, args);
+		LocalTime currentTime = new LocalTime();
+    System.out.println("The current local time is: " + currentTime);
+    Greeter greeter = new Greeter();
+    System.out.println(greeter.sayHello());
 	}
 
 }
--- src/test/java/com/simplilearn/piedpiper/PiedpiperApplicationTests.java
+++ src/test/java/com/simplilearn/piedpiper/PiedpiperApplicationTests.java
@@ -2,9 +2,18 @@ package com.simplilearn.piedpiper;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import static org.hamcrest.CoreMatchers.containsString;
+import static org.junit.Assert.*;
 
 @SpringBootTest
 class PiedpiperApplicationTests {
+
+	private Greeter greeter = new Greeter();
+
+  @Test
+  public void greeterSaysHello() {
+    assertThat(greeter.sayHello(), containsString("Hello"));
+  }
 
 	@Test
 	void contextLoads() {
