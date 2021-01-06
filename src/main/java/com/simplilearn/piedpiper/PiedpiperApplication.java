0cab929965c1e2ef97dfdd78698c60bf319d9214
Ram Iyer <ramky.iyer@gmail.com>
2021-01-06 15:25:34 -0500

    first commit

---
+++ src/main/java/com/simplilearn/piedpiper/PiedpiperApplication.java
@@ -0,0 +1,13 @@
+package com.simplilearn.piedpiper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PiedpiperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PiedpiperApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/simplilearn/piedpiper/PiedpiperApplicationTests.java
@@ -0,0 +1,13 @@
+package com.simplilearn.piedpiper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PiedpiperApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
